e66bde833e8906f3c9035e64ecf2058351ced8a8
Blunde1 <lundeberent@gmail.com>
2019-09-08 20:12:27 +0200

    Updated R-package documentation

--- R-package/R/gbtorch-package.R
+++ R-package/R/gbtorch-package.R
@@ -1,6 +1,19 @@
-#' GBTorch
-#'
-#' GBTorch is a lightning fast gradient boosting library designed to avoid manual tuning and cross-validation by utilizing an information theoretic approach. This makes the algorithm adaptive to the dataset at hand; it is completely automatic, and with minimal worries of overfitting. Consequently, the speed-ups relative to state-of-the-art implementations are in the thousands while mathematical and technical knowledge required on the user are minimized.
+#' \code{gbtorch} is a lightning fast gradient boosting library designed to avoid 
+#' manual tuning and cross-validation by utilizing an information theoretic 
+#' approach. This makes the algorithm adaptive to the dataset at hand; it is 
+#' completely automatic, and with minimal worries of overfitting. 
+#' Consequently, the speed-ups relative to state-of-the-art implementations 
+#' are in the thousands while mathematical and technical knowledge required 
+#' on the user are minimized.
+#' 
+#' Important functions:
+#' 
+#' \itemize{
+#' \item \code{gtb.train}: function for training a \code{gbtorch} ensemble
+#' \item \code{gtb.pred}: function for predicting from a \code{gbtorch} ensemble
+#' }
+#' 
+#' See individual function documentation for usage.
 #'
 #' @docType package
 #' @name gbtorch
