8313081e29b588a88fed8a8c6a7bef587a86507b
Blunde1 <lundeberent@gmail.com>
2019-09-29 21:57:23 +0200

    Illustration of boost from predictions functionality

---
+++ R-package/demo/boost_from_predictions.R
@@ -0,0 +1,43 @@
+# boosted training given previous predictions
+# Berent Lunde
+# 29.09.2019
+
+# Description: Illustrates boosted training given previous predictions from some model.
+# GBTorch will immediately calculate derivatives to train from, and try to find what complexity
+# still not added to the previous model (e.g. non-linearity, interactions, ...)
+# Useful if previous model is relatively simple, e.g. a sparse (regularized) linear model
+
+# Load GBTorch library
+library(gbtorch)
+
+# Simulate y~N(x, 4), x~U(0,4)
+x <- runif(500, 0, 4)
+y <- rnorm(500, x, 1)
+x.test <- runif(500, 0, 4)
+y.test <- rnorm(500, x.test, 1)
+
+# First do standard boosting
+param <- list("learning_rate" = 0.03, "loss_function" = "mse", "nrounds"=2000)
+mod <- gbt.train(param, y, as.matrix(x), F, T)
+mod$get_num_trees()
+y.pred <- predict( mod, as.matrix( x.test ) )
+
+plot(x.test, y.test)
+points(x.test, y.pred, col=2)
+
+# Train a linear model
+lm.mod <- lm(y~., data.frame(y=y, x=x))
+
+# Assume L2 regularized with some strength --> preds scaled with 0.5
+preds <- 0.7* predict(lm.mod, data.frame(x))
+preds.test <- 0.7* predict(lm.mod, newdata=data.frame(x=x.test))
+
+# How does this look like?
+points(x.test, preds.test, col=3)
+
+# Train from regularized linear model
+mod2 <- gbt.train(param, y, as.matrix(x), F, T, preds)
+mod2$get_num_trees() # Smaller than boosting iterations for mod -- less added complexity needed
+
+y.pred2 <- predict( mod2, as.matrix(x.test))
+points(x.test, preds.test + y.pred2, col=4)
