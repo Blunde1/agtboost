4d8c9e0856a3a8ac6f4d84c2065c0fb5cef205ac
Blunde1 <lundeberent@gmail.com>
2019-11-09 20:59:00 +0100

    adding a simple demo script

---
+++ R-package/demo/poisson_regression.R
@@ -0,0 +1,16 @@
+# Poisson regression
+# 09.11.2019
+# Berent Lunde
+
+data(mtcars)
+head(mtcars)
+dim(mtcars)
+
+y.train <- mtcars[,11]
+x.train <- as.matrix(mtcars[,-11])
+
+gbt_mod <- gbt.train(y.train, x.train, loss_function = "poisson", verbose=10)
+pred <- predict(gbt_mod, x.train) # predict log(lambda)
+pred_lambda <- exp(pred) # E[y] = lambda, y~Pois(lambda)
+
+plot(pred_lambda, y.train)
