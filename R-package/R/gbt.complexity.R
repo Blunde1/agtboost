d06008fd6c8c7e904650dfca872956f0dcd272bb
Blunde1 <lundeberent@gmail.com>
2021-10-31 16:32:43 +0100

    Introduce gbt.complexity working solution for xgb and lgb

--- R-package/R/gbt.complexity.R
+++ R-package/R/gbt.complexity.R
@@ -1,5 +1,7 @@
+# Return g^{-1}(preds). 
+# g: implied by loss-function
 transform_prediction <- function(pred, loss_fun_type){
-  # This is default: Predict g^{-1}(preds)
+  # Find the type of transformation
   link_type = ""
   if(loss_fun_type %in% c("mse")){
     link_type = "identity"
@@ -14,6 +16,7 @@ transform_prediction <- function(pred, loss_fun_type){
     warning(paste0("No link-function match for loss: ", loss_fun_type, " Using identity"))
     link_type = "identity"
   }
+  # Transform, given type of transformation
   if(link_type == "identity"){
     res <- pred
   }else if(link_type == "logit"){
@@ -26,6 +29,8 @@ transform_prediction <- function(pred, loss_fun_type){
   return(res)
 }
 
+
+# Return XGBoost loss-function given agtboost loss-function
 loss_to_xgbloss <- function(loss_function){
   if(loss_function == "mse"){
     xgbloss = "reg:squarederror"
@@ -39,6 +44,8 @@ loss_to_xgbloss <- function(loss_function){
   return(xgbloss)
 }
 
+
+# Return LightGBM loss-function given agtboost loss-function
 loss_to_lgbloss <- function(loss_function){
   if(loss_function == "mse"){
     lgbloss = "regression"
@@ -50,6 +57,35 @@ loss_to_lgbloss <- function(loss_function){
     lgbloss = "poisson"
   }
   return(lgbloss)
+}
+
+
+# Obtain agtboost parameters/complexity measurs
+gbt.model.complexity <- function(model){
+    # Setup or get agtboost implicit parameters/complexity measures
+    # # ensemble parameters
+    loss_function = model$get_loss_function()
+    initial_raw_prediction = model$initialPred
+    
+    res <- list(
+      loss_function = loss_function,
+      nrounds = model$get_num_trees(),
+      learning_rate = model$get_learning_rate(),
+      initial_raw_prediction = initial_raw_prediction,
+      initial_prediction = transform_prediction(initial_raw_prediction, loss_function),
+      # # tree parameters
+      max_depth = max(model$get_tree_depths()),
+      min_loss_reductions = model$get_max_node_optimism(),
+      sum_hessian_weights = model$get_min_hessian_weights(),
+      number_of_leaves = max(model$get_num_leaves()),
+      # # objective
+      l1_regularization = 0.0,
+      l2_regularization = 0.0,
+      # # sampling
+      row_subsampling = 1.0,
+      column_subsampling = 1.0
+    )
+    return(res)
 }
 
 #' Return complexity of model in terms of hyperparameters.
@@ -82,64 +118,57 @@ loss_to_lgbloss <- function(loss_function){
 #' @rdname gbt.complexity
 #' @export
 gbt.complexity <- function(model, type){
-  
-  # # ensemble parameters
-  loss_function <- model$get_loss_function()
-  nrounds <- model$get_num_trees()
-  learning_rate <- model$get_learning_rate()
-  initial_raw_prediction <- model$initialPred # needs to be transformed?
-  initial_prediction <- transform_prediction(initial_raw_prediction, loss_function)
-  # # tree parameters
-  max_depth <- max(model$get_tree_depths())
-  min_loss_reductions <- model$get_max_node_optimism()
-  sum_hessian_weights <- model$get_min_hessian_weights()
-  number_of_leaves <- max(model$get_num_leaves())
-  # # objective
-  l1_regularization <- 0.0
-  l2_regularization <- 0.0
-  # # sampling
-  row_subsampling <- 1.0
-  column_subsampling <- 1.0
-  
-  if(type=="xgboost"){
-    parameters = list(
-      # ensemble param
-      "base_score" = initial_prediction,
-      "nrounds" = nrounds,
-      "learning_rate" = learning_rate,
-      # tree param
-      "max_depth" = max_depth,
-      "gamma" = min_loss_reductions,
-      "min_child_weight" = sum_hessian_weights,
-      "max_leaves" = number_of_leaves,
-      "grow_policy" = "lossguide",
-      # objective
-      "objective" = loss_to_xgbloss(loss_function),
-      "alpha" = 0.0,
-      "lambda" = 0.0,
-      # subsampling
-      "subsample" = 1.0,
-      "colsample_bytree" = 1.0
-    )
-  }else if(type=="lightgbm"){
-    parameters = list(
-      # ensemble param
-      "init_score" = initial_prediction,
-      "nrounds" = nrounds,
-      "learning_rate" = learning_rate,
-      # tree param
-      "max_depth" = max_depth,
-      "min_gain_to_split" = min_loss_reductions,
-      "min_sum_hessian_in_leaf" = sum_hessian_weights,
-      "num_leaves" = number_of_leaves,
-      # objective
-      "objective" = loss_to_lgbloss(loss_function),
-      "lambda_l1" = 0.0,
-      "lambda_l2" = 0.0,
-      # subsampling
-      "bagging_fraction" = 1.0,
-      "feature_fraction" = 1.0
-    ) 
-  }
-  return(parameters)
+    # Setup or get agtboost implicit parameters/complexity measures
+    model_complexity <- gbt.model.complexity(model)
+    
+    # Transform parameters/complexity into library-specific parameters
+    attach(model_complexity)
+    if(type=="xgboost"){
+      # Transform agtboost parameters/complexity measures to xgboost parameters
+      warning("Experimental feature")
+      message("Best results are obtained using agtboost algorithm global-subset")
+      parameters = list(
+        # ensemble param
+        "base_score" = initial_prediction,
+        "nrounds" = nrounds,
+        "learning_rate" = learning_rate,
+        # tree param
+        "max_depth" = max_depth,
+        "gamma" = min_loss_reductions,
+        "min_child_weight" = sum_hessian_weights,
+        "max_leaves" = number_of_leaves,
+        "grow_policy" = "lossguide",
+        # objective
+        "objective" = loss_to_xgbloss(loss_function),
+        "alpha" = 0.0,
+        "lambda" = 0.0,
+        # subsampling
+        "subsample" = 1.0,
+        "colsample_bytree" = 1.0
+      )
+    }else if(type=="lightgbm"){
+      # Transform agtboost parameters/complexity measures to lightgbm parameters
+      warning("Experimental feature under rapid development")
+      warning("LightGBM parameters will not produce a good LGBM model object")
+      parameters = list(
+        # ensemble param
+        "init_score" = initial_prediction,
+        "nrounds" = nrounds,
+        "learning_rate" = learning_rate,
+        # tree param
+        "max_depth" = max_depth,
+        "min_gain_to_split" = min_loss_reductions,
+        "min_sum_hessian_in_leaf" = sum_hessian_weights,
+        "num_leaves" = number_of_leaves,
+        # objective
+        "objective" = loss_to_lgbloss(loss_function),
+        "lambda_l1" = 0.0,
+        "lambda_l2" = 0.0,
+        # subsampling
+        "bagging_fraction" = 1.0,
+        "feature_fraction" = 1.0
+      ) 
+    }
+    detach(model_complexity)
+    return(parameters)
 }
